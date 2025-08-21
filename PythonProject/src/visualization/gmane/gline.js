0759007a98b9d1ae476d5980623d47ff25cfc52c
ksayed <ksayed-contractor@momah.gov.sa>
2025-08-21 13:23:44 +0300

    s

---
+++ PythonProject/src/visualization/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','ufp.pt','indiana.edu','berkeley.edu','uct.ac.za','columbia.edu','mac.com','mtu.edu','gmail.com'],
+['2005',57,11,10,12,12,14,13,12,11,10],
+['2006',17,11,12,7,5,3,3,3,1,2]
+];
